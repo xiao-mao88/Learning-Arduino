7ffb3a723a049038f110917bae1d1bcd89af26da
xiao_mao <laurenli045@gmail.com>
2023-05-04 23:03:32 -0400

    how are you feeling today

---
+++ 05MoodIndicator.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+/*materials:
+- arduino board
+- one potentiometer
+- 2 100 microfarad capacitors 
+- one servo motor with a pointer
+
+when you turn the potentiometer, the servo will also spin, causing 
+the pointer to spin; you can add a chart so that the pointer will spin
+to your current mood
+*/
+
+//creates servo object
+Servo myServo;
+
+int const potPin = A0;
+int potVal;
+int angle;
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600);
+}
+
+void loop() {
+  potVal = analogRead(potPin);
+
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+
+  angle = map(potVal, 0, 1023, 0, 179);
+
+  Serial.print(", angle: ");
+  Serial.println(angle);
+
+  myServo.write(angle);
+
+  //wait for servo
+  delay(15);
+}
