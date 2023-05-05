4bdc97833a9cb64863b6563c9cab58e1ff195458
xiao_mao <laurenli045@gmail.com>
2023-05-04 20:15:43 -0400

    3...2...1...take off

---
+++ 02SpaceshipInterface.ino
@@ -0,0 +1,37 @@
+int switchstate = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  pinMode(2, INPUT);
+
+  //3 is the green LED
+  //4 & 5 refer to the two red LEDs
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchstate = digitalRead(2);
+
+  if (switchstate == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }
+
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(250);
+
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+
+    delay(250);
+  }
+}
