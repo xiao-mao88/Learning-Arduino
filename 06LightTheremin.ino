5b8b6abff44f8a04fac56ec439cfa2cc514436dc
xiao_mao <laurenli045@gmail.com>
2023-05-05 15:41:30 -0400

    light gives you sound

---
+++ 06LightTheremin.ino
@@ -0,0 +1,48 @@
+/*materials:
+- arduino board
+- wires (get a few)
+- 1 300 ohm resistor
+- one piezo
+- one photoresistor
+
+when light hits the photoresistor, the piezo will let out a frequency 
+if you cover the photoresistor, then the frequency will be lower
+*/
+
+//variables: hold sensor value, low value, high value
+int sensVal;
+int sensLow = 1023;
+int sensHigh = 0;
+const int ledPin = 13;
+
+void setup() {
+  //LED turns on and the pin is an output
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  //calibration
+  while(millis()<500){
+    sensVal = analogRead(A0);
+
+    if(sensVal > sensHigh){
+      sensHigh = sensVal;
+    }
+
+    if(sensVal < sensLow){
+      sensLow = sensVal;
+    }
+  }
+
+  digitalWrite(ledPin, LOW);
+
+}
+
+void loop() {
+  sensVal = analogRead(A0);
+
+  int pitch = map(sensVal, sensLow, sensHigh, 50, 4000);
+ 
+  tone(8, pitch, 20);
+ 
+  delay(10);
+}
